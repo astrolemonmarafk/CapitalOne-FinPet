296fc8533eac0ae38e8d7b1608f1982c36dc851f
Mar Garza C <125095282+astrolemonmarafk@users.noreply.github.com>
2024-09-15 11:13:04 -0600

    testing zeroshot

--- ai/zeroshot.py
+++ ai/zeroshot.py
@@ -13,8 +13,11 @@ def query(payload):
 
 def zeroshot_classification(product, cost, job, salary, hobbies):
     output = query({
-        "inputs": f"You are tasked with determining whether a transaction is considered an appropiate or irresponsible financial decision. The transaction in question involves purchasing {product} for {cost}, made by a {job} with a {salary} salary and hobbies such as {hobbies}. Keep in mind that not all non-income-generating expenses are inherently bad. Based on this context, classify the transaction.",
+        "inputs": f"You are tasked with determining whether a transaction is considered an appropiate or irresponsible financial decision. The transaction in question involves purchasing {product} for {cost}, made by a {job} with a {salary} salary and hobbies such as {hobbies}. Keep in mind that not all non-income-generating expenses are inherently bad. Based on this context, classify the transaction. Give us a score for both 'good' and 'bad' decisions.",
         "parameters": {"candidate_labels": ["good", "bad"]},
-    })
-
+    })       
     return output
+
+if __name__ == "__main__":
+    print(zeroshot_classification("a car", "10000", "teacher", "50000", "reading and gardening")) 
+    # {'sequence': 'You are tasked with determining whether a transaction is considered an appropiate or irresponsible financial decision. The transaction in question involves purchasing a car for 10000, made by a teacher with a 50000 salary and hobbies such as reading and gardening. Keep in mind that not all non-income-generating expenses are inherently bad. Based on this context, classify the transaction. Give us a score for both "good" and "bad" decisions.', 'labels': ['good', 'bad'], 'scores': [0.9999998807907104, 1.191022724122366e-07]}
